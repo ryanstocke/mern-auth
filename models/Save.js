bc439dd28c28a3f096ee5290ff24748368094c09
ryanstocke <ryanstocke@gmail.com>
2019-02-05 01:21:24 -0700

    working on mongo schema for saving

---
+++ models/Save.js
@@ -0,0 +1,28 @@
+// const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema;
+
+// const saveSchema = new Schema({
+//     char: {
+//         type: mongoose.Schema.ObjectId, 
+//         ref: 'User',
+//         allowNull: false
+//       },
+//       hppotion: {
+//         type: DataTypes.BOOLEAN,
+//         allowNull: false
+//       },
+//       scene: {
+//           type: DataTypes.INTEGER,
+//           allowNull: false
+//       }
+// },{
+//     toJSON: {virtuals: true},
+//     toObject: {virtuals: true}
+// });
+
+
+
+// const Save = mongoose.model("Save", saveSchema);
+
+// module.exports = Save;
